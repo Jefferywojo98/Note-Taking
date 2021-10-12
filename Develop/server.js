55956982cca0c89ebacd4692e43a92657e737bd2
Jeffery <jefferywojo1998@gmail.com>
2021-10-12 11:03:46 -0500

    adding to heroku

--- Develop/server.js
+++ Develop/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const noteData = require('./db/db.json');
 const uuid = require('./helper/uuid');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
